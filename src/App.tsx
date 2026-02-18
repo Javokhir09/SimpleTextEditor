7f38b3ca2b97f4653afe7c016bca1f867e285c35
Javohir <javokhirturdikulov@gmail.com>
2026-02-18 11:30:09 +0500

    save text to localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react"
 import Textarea from "./components/Textarea"
 
 function App() {
+  const [text, setText] = useState<string>("")
+
+  useEffect(() => {
+    const saved = localStorage.getItem("text")
+    if (saved) {
+      setText(saved)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("text", text)
+  }, [text])
+
   return (
     <>
-      <Textarea placeholder="Write your text here..." />
+      <Textarea 
+        placeholder="Write your text here..."
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+      />
     </>
   )
 }
