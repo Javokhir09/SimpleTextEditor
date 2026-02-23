f409c05690522944067cdedf3e18509d2ecc1983
Javohir <javokhirturdikulov@gmail.com>
2026-02-23 12:31:32 +0500

    added more wallpapers

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,15 @@ import bg1 from "./assets/bg-1.jpg";
 import bg2 from "./assets/bg-2.jpg";
 import bg3 from "./assets/bg-3.jpg";
 import bg4 from "./assets/bg-4.jpg";
+import bg5 from "./assets/bg-5.jpg";
+import bg6 from "./assets/bg-6.jpg";
+import bg7 from "./assets/bg-7.jpg";
+import bg8 from "./assets/bg-8.jpg";
+import bg9 from "./assets/bg-9.jpg";
+import bg10 from "./assets/bg-10.jpg";
+import bg11 from "./assets/bg-11.jpg";
+import bg12 from "./assets/bg-12.jpg";
+import bg13 from "./assets/bg-13.jpg";
 import Button from "./components/Button";
 import {
   AArrowDownIcon,
@@ -22,7 +31,7 @@ import { initKeyboardEngine, playKeySound } from "./audio/keyboardSound";
 import jsPDF from "jspdf";
 
 function App() {
-  const wallpapers = [bg1, bg2, bg3, bg4];
+  const wallpapers = [bg1, bg2, bg3, bg4, bg5, bg6, bg7, bg8, bg9, bg10, bg11, bg12, bg13];
   const [text, setText] = useState<string>("");
   const [textSize, setTextSize] = useState<number>(16);
   const [tempTextSize, setTempTextSize] = useState<number>(16);
